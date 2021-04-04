32a070bd0527afd36bfc5747b07a19a3d027c8d7
bibina <bibina.ghale@gmail.com>
2021-04-05 00:07:00 +0545

    login register Integration

---
+++ app/src/main/java/com/example/homeservice/ui/auth/register/UserTypeActivity.java
@@ -0,0 +1,16 @@
+package com.example.homeservice.ui.auth.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.homeservice.R;
+
+public class UserType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_type);
+    }
+}
---
+++ app/src/main/java/com/example/homeservice/ui/splashscreen/SplashScreenActivity.java
@@ -0,0 +1,36 @@
+package com.example.homeservice.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.homeservice.R;
+import com.example.homeservice.helper.Prefs;
+import com.example.homeservice.ui.auth.login.LoginActivity;
+import com.example.homeservice.ui.auth.register.ContactActivity;
+import com.example.homeservice.ui.dashboard.DashboardActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (Prefs.getIsLoggedIn()) {
+                    startActivity(new Intent(SplashScreenActivity.this, DashboardActivity.class));
+                } else {
+                    startActivity(new Intent(SplashScreenActivity.this, ContactActivity.class));
+                }
+                finish();
+            }
+        }, 3000);
+    }
+}
